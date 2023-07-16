b78800f91d97fe1c251976e205d2071171fe97b6
Shaun Wah <me@shaunwah.com>
2023-07-17 06:16:15 +0800

    Fix argument description

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ parser.add_argument("--delay", type=int, default=0, help="adds a delay to the fi
 parser.add_argument("--offset", type=int, default=0, help="adds an offset to all thumbnails")
 parser.add_argument("--rows", type=int, default=5, help="number of rows of generated thumbnails")
 parser.add_argument("--columns", type=int, default=4, help="number of columns of generated thumbnails")
-parser.add_argument("--resize", type=int, default=100, help="resizes thumbnails (1-100%)")
+parser.add_argument("--resize", type=int, default=100, help="resizes thumbnails (1-100 percent)")
 parser.add_argument("--override", action="store_true", help="overrides existing content")
 parser.add_argument("--safe", action="store_true", help="enables safe mode")
 args = parser.parse_args()
